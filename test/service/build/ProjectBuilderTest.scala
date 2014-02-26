00740407253445ffe13df0ef2a8a0bf64978cb11
ubourdon <bourdon.ugo@gmail.com>
2014-02-26 01:59:00 +0100

    clean: ProjectBuilderTest

--- test/service/build/ProjectBuilderTest.scala
+++ test/service/build/ProjectBuilderTest.scala
@@ -1,15 +1,16 @@
 package service.build
 
-import org.scalatest.{BeforeAndAfter, BeforeAndAfterAll, FunSuiteLike, Matchers}
+import org.scalatest._
 import akka.testkit.{TestProbe, TestActorRef, TestKit}
 import akka.actor.{ActorRefFactory, ActorSystem}
-import model.Project
 import scalax.file.Path
 import testing.tools.ActorTestingTools
 import model.reactive.event.EventProducer
 import org.scalatest.mock.MockitoSugar
 import play.api.libs.iteratee.Concurrent.Channel
 import org.mockito.Mockito._
+import model.Project
+import service.build.SubBuilderFactory.SubBuilderFactory
 
 class ProjectBuilderTest extends TestKit(ActorSystem("ProjectBuilderTest"))
                               with FunSuiteLike with Matchers with BeforeAndAfterAll
@@ -20,36 +21,57 @@ class ProjectBuilderTest extends TestKit(ActorSystem("ProjectBuilderTest"))
     after { closeDummyActors(ProjectBuilder.name) }
 
     test("when receive LaunchProjectBuild should launch LaunchSubBuild(project) to all SubBuilder (2)") {
-        val metadataProbe = TestProbe()
         val testCodeCoverageProbe = TestProbe()
         val checkstyleProbe = TestProbe()
 
-        val subBuilderFactoryStub = { (subBuilder: SubBuilderName) =>
+        val (stubEventProvider, _) = eventProducer()
+
+        val underTest = TestActorRef(ProjectBuilder.props(subBuilderFactoryStub(testCodeCoverageProbe, checkstyleProbe), stubBashExecutor), ProjectBuilder.name)
+
+        underTest ! LaunchProjectBuild(Project("project", "gitUrl", Path("")), stubEventProvider)
+
+        val msg = LaunchSubBuild(Project("project", "gitUrl", Path("thePath")))
+
+        testCodeCoverageProbe   expectMsg  msg
+        checkstyleProbe         expectMsg  msg
+    }
+
+    test("when receive LaunchProjectBuild should push ProjectCloned(project) into corresponding event channel  ") {
+        val testCodeCoverageProbe = TestProbe()
+        val checkstyleProbe = TestProbe()
+
+        val (stubEventProvider, stubChannel) = eventProducer()
+
+        val underTest = TestActorRef(ProjectBuilder.props(subBuilderFactoryStub(testCodeCoverageProbe, checkstyleProbe), stubBashExecutor), ProjectBuilder.name)
+
+        underTest ! LaunchProjectBuild(Project("project", "gitUrl", Path("")), stubEventProvider)
+
+        verify(stubChannel).push(ProjectCloned(Project("project", "gitUrl", Path(""))))
+    }
+
+    private def eventProducer(): (EventProducer[ProjectBuildEvent], Channel[ProjectBuildEvent]) = {
+        val stubChannel = mock[Channel[ProjectBuildEvent]]
+        val stubEventProvider = mock[EventProducer[ProjectBuildEvent]]
+        when(stubEventProvider.channel).thenReturn(stubChannel)
+
+        (stubEventProvider, stubChannel)
+    }
+
+    private def subBuilderFactoryStub(testCodeCoverageProbe: TestProbe, checkstyleProbe: TestProbe): SubBuilderFactory = {
+        (subBuilder: SubBuilderName) => {
             val subBuilderInstance = subBuilder match {
-                case MetadataProjectSubBuilderName => new MetadataProjectSubBuilder(metadataProbe.ref) 
                 case TestCodeCoverageSubBuilderName => new TestCodeCoverageSubBuilder(testCodeCoverageProbe.ref)
                 case CheckstyleSubBuilderName => new CheckstyleSubBuilder(checkstyleProbe.ref)
             }
 
             (_: ActorRefFactory) => subBuilderInstance
         }
-
-        val stubEventProvider = mock[EventProducer[ProjectBuildEvent]]
-        val stubChannel = mock[Channel[ProjectBuildEvent]]
-        when(stubEventProvider.channel).thenReturn(stubChannel)
-
-        val underTest = TestActorRef(ProjectBuilder.props(subBuilderFactoryStub, StubBashExecutor), ProjectBuilder.name)
-
-        underTest ! LaunchProjectBuild(Project("project", "gitUrl", Path("")), stubEventProvider)
-
-        val msg = LaunchSubBuild(Project("project", "gitUrl", Path("thePath")))
-        
-//        metadataProbe           expectMsg  msg
-        testCodeCoverageProbe   expectMsg  msg
-        checkstyleProbe         expectMsg  msg
     }
-}
 
-object StubBashExecutor extends BashExecutor {
-    def gitCloneProject(project: Project): Path = Path("thePath")
+    private val stubBashExecutor: BashExecutor = {
+        val stubBashExecutor = mock[BashExecutor]
+        when(stubBashExecutor.gitCloneProject(Project("project", "gitUrl", Path("")))).thenReturn(Path("thePath"))
+
+        stubBashExecutor
+    }
 }
